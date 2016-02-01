6592743e8c1e867cab8dcd7a86090bc788916b6d
Alon Niv <alon@cybereason.com>
2016-02-01 10:03:26 +0200

    add docs

--- pyvue/page.py
+++ pyvue/page.py
@@ -3,24 +3,44 @@ from functools import partial
 
 
 def format_tag(template, tag_name, attrs):
+    """
+    Format a template of a tag with attributes, so that:
+    >>> format_tag('<{} {}>', 'span', {'id': 'foo'})
+    >>> '<span id="foo">'
+    Note: use `klass` instead of the `class` attribute.
+    """
     return template.format(tag_name, ' '.join(['{}="{}"'.format(key.replace('klass', 'class'), val) 
                                                for (key, val) in attrs.iteritems()]))
 
 
 def tag(tag_name, bufferer):
+    """
+    Creates a tag factory used via context manager.
+    DO NOT USE DIRECTLY.
+    >>> with tag('span', page)({'id': 'foo'}):
+        ... page.text('foo!')
+    >>> str(page)
+    >>> '<span id="foo">foo!</span>'
+    """
     @ctx.contextmanager
     def factory(**attrs):
-        bufferer._buffer += format_tag('<{} {}>', tag_name, attrs)
+        bufferer += format_tag('<{} {}>', tag_name, attrs)
         yield
-        bufferer._buffer += '</{}>'.format(tag_name)
+        bufferer += '</{}>'.format(tag_name)
     return factory
 
 
 class Page(object):
+    """
+    Create a page buffer.
+    """
     def __init__(self):
         self._buffer = ''
 
     def __getattr__(self, attr):
+        """
+        This creates the magic of tag factories.
+        """
         if attr.startswith('_') or attr == 'text': 
             return super(Page, self).__getattr__(self, attr)
         elif attr in ('input', 'img', 'hr'):
@@ -30,6 +50,10 @@ class Page(object):
     def __str__(self):
         return self._buffer
 
+    def __iadd__(self, text):
+        self._buffer += text
+        return self
+
     def _self_closing_tag(self, tag_name, **attrs):
         self._buffer += format_tag('<{} {}/>', tag_name, attrs)
 
@@ -38,6 +62,24 @@ class Page(object):
 
 
 def view(f):
+    """
+    Decorate a view function.
+    The decorated function has to recieve a Page object
+    as a first parameter.
+    The resulting function will recieve all parameters *except*
+    the Page object, and will always return the rendered Page.
+
+    Example usage:
+
+    @view
+    def main(p, title):
+        with p.div(klass='container'):
+            with p.h1():
+                p.text(title)
+
+    >>> main('Foo')
+    >>> '<div class="container"><h1>Foo</h1></div>'
+    """
     def _inner(*args, **kwargs):
         p = Page()
         f(p, *args, **kwargs)
--- pyvue/parser.py
+++ pyvue/parser.py
@@ -1,3 +1,41 @@
+"""
+The pyvue syntax is a superset of python, allowing for a new
+type of function declaration. Essentially, it adds the `view` keyword,
+which becomes a very specialized type of `def`, and allows you to define
+view functions.
+A view finction uses a mixture of python code and HTML markup,
+and returns a rendered Page when called. The only rendered elements are
+the ones written in HTML, but they are directly affected by the python code.
+Example:
+
+view main(title):
+    <div klass="foo">
+        <h1>
+            <py>title</py>
+        </h1>
+        <ul>
+            for x in xrange(10):
+                <li>
+                    <py>x</py>
+                </li>
+        </ul>
+    </div>
+
+Notes:
+- A view function has to have a blank line or EOF after it
+- A view fuunction mustn't have blank lines in its body
+- Any untagged line is ALWAYS a python line
+- Adding "text" to the page will be done using the `py` tag,
+  and will actually inject python code as text, so contant text
+  should be a string expression: <py>'FOO'</py>
+  All objects passed to the `py` tag will be converted to strings via str().
+- `py` is the only tag that can be used inline, other tags must
+  be used like this:
+  <div>
+      ...
+  </div> # closing tag on its own line with the same indentation as 
+  the openning tag.
+"""
 import re
 
 
@@ -6,6 +44,11 @@ VIEW_HEADER_RE = re.compile('(\s*)view (\w+)\((.*?)\):')
 
 
 def view_parser(content):
+    """
+    Parses pyvue syntax into normal python code.
+    This will NOT inject the view decorator into your code,
+    which you will have to import explicitly.
+    """
     for view_block in [res[0] for res in VIEW_RE.findall(content)]:
         block = VIEW_HEADER_RE.sub(r'\1@view\n\1def \2(__p, \3):', view_block)
         block = re.sub(r'\<py\>((.|\n)*?)\</py\>', r'__p.text(\1)', block)
